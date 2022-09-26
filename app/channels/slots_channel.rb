cf53e7c260b6e7b0b60696841be8cb3b7e61d1f2
Baber Javed <baberjaved.rs@gmail.com>
2022-09-26 05:33:18 -0500

    Confirgured ActionCable

---
+++ app/channels/slots_channel.rb
@@ -0,0 +1,9 @@
+class SlotsChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "slot_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
